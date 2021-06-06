ac24a1fdbd5f6ccb58d9481dbd4049603a8e2591
natalia <53310862+ripleycmd@users.noreply.github.com>
2021-06-06 23:33:27 +0100

    Added amplify backend.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import config from '../src/aws-exports';
+
+Amplify.configure({
+  ...config, ssr: true
+});
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+
 
 export default MyApp
