5de1e72e4ff1f7b56159f2af9b72b7bc8b3262b1
natalia <53310862+ripleycmd@users.noreply.github.com>
2021-06-06 22:39:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          My Next.js Amplify app 
+          My Next.js Amplify app with SSR
         </h1>
       </main>
     </div>
